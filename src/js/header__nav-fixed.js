8a31a57aa3a353991815a5afbffe60952fabf15a
Nikita Babko <niki-babko@yandex.ru>
2020-12-18 15:55:15 +0700

    .psd deleted.

--- src/js/header__nav-fixed.js
+++ src/js/header__nav-fixed.js
@@ -1,7 +1,7 @@
 // //============== Nav-bar fixed ============================//
 const header = document.querySelector(".header");
 
-window.onscroll = function() {
+window.onscroll = function () {
     if (window.pageYOffset > 0) {
         header.classList.add("header--fixed");
     } else {
